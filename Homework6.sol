fad20778ebe483966f7744bd9a0ddba15415b0c9
CumberlandBrace02 <101621112+CumberlandBrace02@users.noreply.github.com>
2023-11-21 08:37:28 +0100

    Update Homework6.sol

--- Homework6.sol
+++ Homework6.sol
@@ -1,1 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract TokenVesting is Ownable {
+    using SafeMath for uint256;
+
+    IERC20 public token;
+    address public beneficiary;
+    uint256 public cliff;
+    uint256 public start;
+    uint256 public duration;
+    uint256 public released;
+
+    event TokensReleased(uint256 amount);
+
+    constructor(
+        IERC20 _token,
+        address _beneficiary,
+        uint256 _start,
+        uint256 _cliffDuration,
+        uint256 _duration
+    ) {
+        require(_beneficiary != address(0), "Invalid beneficiary address");
+        require(_cliffDuration <= _duration, "Cliff duration must be less than or equal to the vesting duration");
+
+        token = _token;
+        beneficiary = _beneficiary;
+        start = _start;
+        cliff = _start.add(_cliffDuration);
+        duration = _duration;
+    }
+
 
